31bb1d7fffdc94f736340313d703d49c9d7f9f01
Ray Villalobos <ray@planetoftheweb.com>
2014-05-20 06:38:19 -0700

    Used data to populate artwork list

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var appdata = require('../data.json');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var myArtwork = [];
+  appdata.speakers.forEach(function(item) {
+    myArtwork = myArtwork.concat(item.artwork);
+  });
+  res.render('index', {
+    title: 'Home',
+    artwork: myArtwork
+  });
 });
 
 module.exports = router;
