9e3c80f8987bd9896da563e9121590fc6dfc7640
Ray Villalobos <ray@planetoftheweb.com>
2014-05-20 06:40:13 -0700

    Added speakers template and updated index route

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,34 @@ router.get('/', function(req, res) {
   });
 });
 
+/* GET speakers page. */
+router.get('/speakers', function(req, res) {
+  var myArtwork = [];
+  appdata.speakers.forEach(function(item) {
+    myArtwork = myArtwork.concat(item.artwork);
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork
+  });
+});
+
+
+/* GET speakers page. */
+router.get('/speakers/:speakerid', function(req, res) {
+  var myArtwork = [];
+  appdata.speakers.forEach(function(item) {
+    if (item.shortname == req.params.speakerid) {
+      myArtwork = myArtwork.concat(item.artwork);
+    }
+  });
+  res.render('speakers', {
+    title: 'Speakers',
+    artwork: myArtwork
+  });
+});
+
+
+
+
 module.exports = router;
